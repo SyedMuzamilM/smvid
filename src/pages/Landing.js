91ee9674e96abe3a708f5c90fa8e07e57f62f8e9
Syed Muzamil <smmhd121@gmail.com>
2021-03-22 19:50:26 +0530

    Initial commit

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -7,11 +7,11 @@ export default function Landing() {
   return (
     <LandingLayout>
       <Hero
-        title="Build this rad landing page from scratch"
-        subtitle="This is the subheader section where you describe the basic benefits of your product"
+        title="Download the YouTube videos with the highest quality."
+        subtitle="You can download any video from the YouTube with 4K quality if available for that video."
         image="https://source.unsplash.com/collection/404339/800x600"
-        ctaText="Create your account now"
-        ctaLink="/signup"
+        ctaText="Download the Video"
+        ctaLink="/download/search"
       />
     </LandingLayout>
   );
